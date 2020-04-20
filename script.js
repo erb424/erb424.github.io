7db1201dea5006e7c3d9873f5f367f4854c76b44
erb424 <56443064+erb424@users.noreply.github.com>
2020-04-20 11:41:55 +0100

    test again

--- script.js
+++ script.js
@@ -60,9 +60,10 @@ function loadExercise() {
 function renderPage(){
 
     if(audio.backend.supportsWebAudio()){
+        document.getElementById("loading").innerHTML = "<h1>TRUE</h1>"
         audio.load(localStorage.media);
         audio.on('ready', function(){
-            document.getElementById("loading").innerHTML = "<h1>TRUE</h1>";
+            /*document.getElementById("loading").innerHTML = "";*/
         })
     }
     else{
